7b20d1ee0e1577a85ce93fcd488b8fee4cb239ba
Hastein <68847620+Ewolucion@users.noreply.github.com>
2021-12-03 11:40:08 +0100

    Add files via upload

---
+++ firma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int lng;
+    float s, P;
+    cout<<"Wprowadz liczbe nadgodzin: ";
+    cin>>lng;
+    cout<<"Wprowadz stawke za godzine: ";
+    cin>>s;
+    if(lng<=30){
+        P = lng * s;
+    }
+    else {
+        P = lng * s + (lng - 30)*s*0.5;
+    };
+    cout<<"Wyplata wynosi: "<<P;
+}
