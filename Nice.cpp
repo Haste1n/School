8d0b58704b6e16a985e2f0bbfd2449381617eb7e
Hastein <68847620+HasteinTPM@users.noreply.github.com>
2022-04-08 10:48:08 +0200

    Update Nice.cpp

--- Nice.cpp
+++ Nice.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
 
-const int M = 6;
-int tab[M];
-int male[M/2], duze[M/2];
-
-void porownaj(){
+void porownaj(int M, int tab[], int duze[], int male[]){
     for (int i = 0; i < M-1; i+=2){
         if (tab[i] > tab[i + 1]){
             duze[i / 2] = tab[i];
@@ -17,14 +13,14 @@ void porownaj(){
         }
     }
 }
-void wprowadz(){
+void wprowadz(int M, int tab[]){
     for (int i = 1; i <= M; i++){
         cout<<"Podaj liczbe "<<i<<": ";
         cin>>tab[i - 1];
     }
 }
 
-void wypisz(){
+void wypisz(int M, int duze[], int male[]){
     for (int i = 1; i <= M/2; i++){
         if (duze[i - 1] == male[i - 1]){
             cout<<"\n"<<duze[i - 1]<<" = "<<male[i - 1];
@@ -35,20 +31,28 @@ void wypisz(){
     }
 }
 
-void maxmin(){
+void maxmin(int M, int tab[], int duze[], int male[]){
     int najmax = duze[0], najmin = male[0];
     for (int i = 1; i < M/2; i++){
         if (duze[i] > najmax) najmax = duze[i];
         if (male[i] < najmin) najmin = male[i];
     }
+    if (tab[M - 1] > najmax) najmax = tab[M - 1];
+    if (tab[M - 1] < najmin) najmin = tab[M - 1];
     cout<<"\n\nNajmniejsze: "<<najmin;
     cout<<"\nNajwieksze: "<<najmax<<endl;
 }
 
 int main(){
-    wprowadz();
-    porownaj();
-    wypisz();
-    maxmin();
+    int a;
+    cout<<"Podaj dlugosc tablicy: ";
+    cin>>a;
+    const int M = a;
+    int tab[M];
+    int male[M/2], duze[M/2];
+    wprowadz(M, tab);
+    porownaj(M, tab, duze, male);
+    wypisz(M, duze, male);
+    maxmin(M, tab, duze, male);
     return 0;
 }
