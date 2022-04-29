67015ef7dd5e6b79f8a979e1ed69865bb9753de7
Hastein <68847620+HasteinTPM@users.noreply.github.com>
2022-04-29 10:48:17 +0200

    Update silnia.cpp

--- silnia.cpp
+++ silnia.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     double long a = -1;
-    cout<<"Program po podaniu liczby liczy jej silnie."<<endl;
+    cout<<"Program po podaniu liczby liczy jej silnie.\nDziala na liczby w przedziale <0, 1754>."<<endl;
     while (a < 0){
         cout<<"Liczba musi byc wieksza od zera.\n"<<endl;
         cout<<"Podaj liczbe: ";
