bd3ce0bce43b60f602660d802ce01dcf4a03c99e
Hastein <68847620+HasteinTPM@users.noreply.github.com>
2022-04-29 10:39:33 +0200

    Create silnia.cpp

---
+++ silnia.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double long a = -1;
+    cout<<"Program po podaniu liczby liczy jej silnie."<<endl;
+    while (a < 0){
+        cout<<"Liczba musi byc wieksza od zera.\n"<<endl;
+        cout<<"Podaj liczbe: ";
+        cin>>a;
+    }
+    if (a == 0) a = 1;
+    for (int i = a - 1; i > 1; i--) a *= i;
+    cout<<"Silnia rowna: "<<a;
+}
