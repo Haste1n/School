1963f1f2e282cf1813e6db28bdba9ea1202f0ea9
Hastein <68847620+HasteinTPM@users.noreply.github.com>
2022-10-23 17:29:47 +0200

    Add files via upload

---
+++ kolzad.py
@@ -0,0 +1,40 @@
+result = ""
+wejscie = input().split(" ")
+m = int(wejscie[0])
+p = int(wejscie[1])
+n = int(wejscie[2])
+wejsciep = []
+wejscien = []
+wonsz = [(0, 0)]
+for i in range(1, p+1):
+    wejsciep.append(input().split(" "))
+for i in range(p+1, p+n+1):
+    wejscien.append(input().split(" "))
+plansza = []
+for i in range(0, m):
+    plansza.append([])
+    for l in range(0, m):
+        plansza[i].append(None)
+for i in range(0, len(wejsciep)):
+    plansza[int(wejsciep[i][0])-1][int(wejsciep[i][1])-1] = wejsciep[i][2]
+plansza[0][0] = "0"
+for i in range(0, len(wejscien)):
+    if wejscien[i][0] == "Z":
+        if (int(wejscien[i][1]) - 1, int(wejscien[i][2]) - 1) in wonsz:
+            print(int(plansza[int(wejscien[i][1]) - 1][int(wejscien[i][2]) - 1]))
+        else:
+            print(-1)
+    else:
+        if wejscien[i][0] == "G":
+            wonsz.append((wonsz[-1][0] - 1, wonsz[-1][1]))
+        elif wejscien[i][0] == "D":
+            wonsz.append((wonsz[-1][0] + 1, wonsz[-1][1]))
+        elif wejscien[i][0] == "L":
+            wonsz.append((wonsz[-1][0], wonsz[-1][1] - 1))
+        else:
+            wonsz.append((wonsz[-1][0], wonsz[-1][1] + 1))
+        if not plansza[wonsz[-1][0]][wonsz[-1][1]]:
+            for l in range(1, len(wonsz)):
+                plansza[wonsz[len(wonsz) - l][0]][wonsz[len(wonsz) - l][1]] = plansza[wonsz[len(wonsz) - l-1][0]][wonsz[len(wonsz) - l-1][1]]
+                plansza[wonsz[len(wonsz) - l - 1][0]][wonsz[len(wonsz) - l - 1][1]] = None
+            wonsz.pop(0)
