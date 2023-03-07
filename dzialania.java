5fdda2d6bab066a11fa95703c5fda7a10d83bd4d
Hastein <68847620+HasteinTPM@users.noreply.github.com>
2023-03-07 12:40:34 +0100

    Create dzialania.java

---
+++ dzialania.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String args[]) {
+        float a=7, b=5;
+        System.out.println("Suma wynosi: "+(a+b));
+        System.out.println("Różnica wynosi: "+(a-b));
+        System.out.println("Iloczyn wynosi: "+(a*b));
+        System.out.println("Iloraz wynosi: "+(a/b));
+        System.out.println("Reszta z dzielenia wynosi: "+(a%b));
+    }
+}
