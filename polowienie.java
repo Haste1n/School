85de8027e1d01e9b083955f3aa8eb5ccd773314e
Hastein <68847620+HasteinTPM@users.noreply.github.com>
2023-03-28 12:58:56 +0200

    Update polowienie.java

--- polowienie.java
+++ polowienie.java
@@ -2,24 +2,27 @@ import java.util.Scanner;
 
 class polowka {
     public static void main(String[] args) {
-        double e, po=-100, k=100, a=2, b=1, c, s, l, p;
+        double e, po=-100, k=100, a=4, b=6, c, s, l, p;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj epsilon: ");
         e = scanner.nextDouble();
         s = (po+k)/2;
         l = po;
         p = k;
-        while((a*s+b)!=0 || Math.abs(po-k)>=e){
+        while((funkcja(s, a, b))!=0 && Math.abs(po-k)>=e){
             s = polowka(s, l, p, a, b);
-            c = Math.abs(l-p);
+            c = (Math.abs(l-p))/4;
             l = s-c;
             p = s+c;
         }
         System.out.println("Wynik policzony z dokladnoscia: ["+e+"]. Wynosi: "+s);
     }
+    public static double funkcja(double s, double a, double b){
+        return a*s+b;
+    }
     public static double polowka(double s, double l, double p, double a, double b) {
         double po, k;
-        if(a*s+b>0){
+        if(funkcja(s, a, b)>0){
             po = l;
             k = s;
         }
