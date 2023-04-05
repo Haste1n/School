963048fcef436505024a2774a2dc0900862f382c
Hastein <68847620+HasteinTPM@users.noreply.github.com>
2023-04-05 09:37:48 +0200

    Update suma_cyfr_liczby.java

--- suma_cyfr_liczby.java
+++ suma_cyfr_liczby.java
@@ -1,12 +1,18 @@
-class polowienie {
+import java.util.Scanner;
+
+
+class suma_cyfr {
     public static void main(String[] args) {
-        int a = 74852, i = 0, result = 0; 
+        Scanner scanner = new Scanner(System.in);
+        int a, i = 0, result = 0;
+        System.out.println("Podaj liczbe: ");
+        a = scanner.nextInt();
         double s = 0;
         while(Math.pow(10, i)<=a){
             s = a % Math.pow(10, i);
             result += ((a-s) % Math.pow(10, i+1)) / Math.pow(10, i);
             i++;
         }
-        System.out.println(result);
+        System.out.println("Suma cyfr: "+result);
     }
 }
